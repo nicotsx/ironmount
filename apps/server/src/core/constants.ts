481eb13a447cf65c059a072fe16ca4fcc2746a20
Nicolas Meienberger <github@thisprops.com>
2025-09-27 00:39:29 +0200

    chore: use /var/lib/docker/volumes mountpoint

--- apps/server/src/core/constants.ts
+++ apps/server/src/core/constants.ts
@@ -1,3 +1,3 @@
 export const OPERATION_TIMEOUT = 5000;
-export const VOLUME_MOUNT_BASE = "/mounts";
+export const VOLUME_MOUNT_BASE = "/var/lib/docker/volumes/ironmount";
 export const DATABASE_URL = "/data/ironmount.db";
